c539f81f3ca74734d59dc940fa3f3f39e34f6919
MarcoQin <qyyfy2009@163.com>
2016-11-27 15:54:15 +0800

    some test

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,33 @@
 #include <QtWidgets>
 #include "vrenderer.h"
 
+class Test: public QWidget
+{
+public:
+    Test(QWidget *parent) : QWidget(parent)
+    {
+        QLabel *lb = new QLabel("Pushhaha", this);
+        QPushButton *btn = new QPushButton(this);
+    }
+
+};
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     VRenderer vrenderer;
     AVPlayer player;
     player.setRenderer(&vrenderer);
-    vrenderer.show();
 
+
+    QVBoxLayout *layout = new QVBoxLayout();
+    Test *wdt = new Test(NULL);
+    layout->addWidget(vrenderer.widget());
+    layout->addWidget(new Test(NULL));
+    wdt->setLayout(layout);
+//    QLabel *dst = new QLabel("PushThis", vrenderer.widget());
     player.play("/Users/marcoqin/marco/01.mp4");
-
+    wdt->show();
+//    vrenderer.show();
     return a.exec();
 }
