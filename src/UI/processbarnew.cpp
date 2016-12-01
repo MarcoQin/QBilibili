91618d7ee9486e2a4168dcf5775b0c721f3089f4
MarcoQin <qyyfy2009@163.com>
2016-12-01 15:04:42 +0800

    processbarnew

---
+++ src/UI/processbarnew.cpp
@@ -0,0 +1,6 @@
+#include "processbarnew.h"
+
+ProcessBarNew::ProcessBarNew()
+{
+
+}
---
+++ src/UI/processbarnew.h
@@ -0,0 +1,11 @@
+#ifndef PROCESSBARNEW_H
+#define PROCESSBARNEW_H
+
+
+class ProcessBarNew : public FloatWidget
+{
+public:
+    ProcessBarNew();
+};
+
+#endif // PROCESSBARNEW_H
