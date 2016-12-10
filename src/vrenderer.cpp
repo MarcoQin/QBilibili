4c8a64da69f5fab13562108a5d9cddd888a9edb2
MarcoQin <qyyfy2009@163.com>
2016-12-10 12:39:44 +0800

    render method change test

--- src/vrenderer.cpp
+++ src/vrenderer.cpp
@@ -3,19 +3,31 @@
 #include <Qt>
 #include <QtWidgets>
 
+VRenderer* VRenderer::_instance = nullptr;
+
 VRenderer::VRenderer(QWidget *parent)
     :GLWidgetRenderer2(parent)
 {
     setMouseTracking(true);
     setGeometry(0, 0, 800, 600);
     frameTime.start();
-    connect(&timer, SIGNAL(timeout()), this, SLOT(updateGL()));
+    // connect(&timer, SIGNAL(timeout()), this, SLOT(updateGL()));
+    connect(&timer, SIGNAL(timeout()), this, SLOT(w_draw()));
     timer.setInterval(17);
     timer.start();
+    _instance = this;
+}
+
+VRenderer *VRenderer::instance()
+{
+    return _instance ? _instance : new VRenderer(NULL);
 }
 
 void VRenderer::paintGL()
 {
+    if (1 < 2) {
+        return;
+    }
     ++frameCount;
     double fps = frameCount / ((double)frameTime.elapsed() / 1000.0);
     // mix gl and qpainter
@@ -72,3 +84,74 @@ void VRenderer::paintGL()
     swapBuffers();
 }
 
+void VRenderer::enterDraw(QPainter &painter)
+{
+    painter.beginNativePainting();
+    handlePaintEvent();
+}
+
+void VRenderer::exitDraw(QPainter &painter)
+{
+    painter.endNativePainting();
+    swapBuffers();
+}
+
+void VRenderer::w_draw()
+{
+    ++frameCount;
+    double fps = frameCount / ((double)frameTime.elapsed() / 1000.0);
+    // mix gl and qpainter
+    QPainter painter(this);
+    enterDraw(painter);
+    // enter draw context
+    // painter.beginNativePainting();
+
+    // video draw stuffs
+    // handlePaintEvent();
+
+    // custom draw stuffs
+    // ---- draw normal text
+    int alpha = 100;
+    painter.setPen(QColor(100, 100, 200, 255));
+    QFont font;
+    font.setPixelSize(24);
+    painter.setFont(font);
+    painter.setRenderHints(QPainter::Antialiasing | QPainter::TextAntialiasing);
+    painter.drawText(10, 10, "Hello World");
+    painter.drawLine(0, 0, 400, 400);
+    painter.drawText(100, 100, "你好世界");
+    // ---- draw text with outline
+    QPainterPath path;
+    QPen pen;
+    QPoint posToDraw(50, 132);
+    font.setWeight(QFont::Weight::Normal);
+    pen.setWidth(5);
+    pen.setColor(QColor(0, 0, 0, alpha));
+    painter.setFont(font);
+    painter.setPen(pen);
+
+    const QString textToDraw("中文And English with Σ(*ﾟдﾟﾉ)ﾉ...");
+    path.addText(posToDraw,font, textToDraw);
+    painter.drawPath(path);
+    pen.setColor(QColor(255, 255, 255, alpha));
+    painter.setPen(pen);
+    painter.drawText(posToDraw, textToDraw);
+
+
+    QFontMetrics fm(font);
+    int pixelsWide = fm.width(textToDraw);
+    int piexelsHigh = fm.height();
+    // qDebug() << pixelsWide << piexelsHigh;
+    // qDebug() << width() ;
+
+    painter.drawText(QPoint(300, 100), QString::number(fps));
+//    painter.drawText(QPoint(350, 100), QString::number(frameCount));
+//    painter.drawText(QPoint(400, 100), QString::number(frameTime.elapsed()));
+
+    // leave draw context
+    // painter.endNativePainting();
+
+    // swap gl buffer
+    // swapBuffers();
+    exitDraw(painter);
+}
--- src/vrenderer.h
+++ src/vrenderer.h
@@ -8,14 +8,21 @@ using namespace QtAV;
 
 class VRenderer : public GLWidgetRenderer2
 {
+    Q_OBJECT
 public:
     VRenderer(QWidget *parent);
+    static VRenderer *instance();
+    void enterDraw(QPainter &painter);
+    void exitDraw(QPainter &painter);
 protected:
     void paintGL();
 private:
+    static VRenderer *_instance;
     QTime frameTime;
     QTimer timer;
     long frameCount = 0;
+private slots:
+    void w_draw();
 };
 
 #endif // VRENDERER_H
