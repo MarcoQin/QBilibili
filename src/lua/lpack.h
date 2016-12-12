ed9a9402b326f7af0d089107285a6be89ca20c62
MarcoQin <qyyfy2009@163.com>
2016-12-12 11:18:16 +0800

    lpack complete

--- src/lua/lpack.h
+++ src/lua/lpack.h
@@ -1,4 +1,12 @@
 #ifndef LPACK_H
 #define LPACK_H
 
+#include <ctype.h>
+#include <string.h>
+
+#include "lua.h"
+#include "lualib.h"
+#include "lauxlib.h"
+int luaopen_pack(lua_State *L);
+
 #endif // LPACK_H
