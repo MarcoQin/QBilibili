6e3f73ff814a185fb66617ff6548410b176a415c
MarcoQin <qyyfy2009@163.com>
2016-12-12 11:26:16 +0800

    clean code

--- src/vrenderer.cpp
+++ src/vrenderer.cpp
@@ -26,63 +26,9 @@ VRenderer *VRenderer::instance()
 
 void VRenderer::paintGL()
 {
-    if (1 < 2) {
-        return;
-    }
-    ++frameCount;
-    double fps = frameCount / ((double)frameTime.elapsed() / 1000.0);
-    // mix gl and qpainter
-    QPainter painter(this);
-    // enter draw context
-    painter.beginNativePainting();
-
-    // video draw stuffs
-    handlePaintEvent();
-
-    // custom draw stuffs
-    // ---- draw normal text
-    int alpha = 100;
-    painter.setPen(QColor(100, 100, 200, 255));
-    QFont font;
-    font.setPixelSize(24);
-    painter.setFont(font);
-    painter.setRenderHints(QPainter::Antialiasing | QPainter::TextAntialiasing);
-    painter.drawText(10, 10, "Hello World");
-    painter.drawLine(0, 0, 400, 400);
-    painter.drawText(100, 100, "你好世界");
-    // ---- draw text with outline
-    QPainterPath path;
-    QPen pen;
-    QPoint posToDraw(50, 132);
-    font.setWeight(QFont::Weight::Normal);
-    pen.setWidth(5);
-    pen.setColor(QColor(0, 0, 0, alpha));
-    painter.setFont(font);
-    painter.setPen(pen);
-
-    const QString textToDraw("中文And English with Σ(*ﾟдﾟﾉ)ﾉ...");
-    path.addText(posToDraw,font, textToDraw);
-    painter.drawPath(path);
-    pen.setColor(QColor(255, 255, 255, alpha));
-    painter.setPen(pen);
-    painter.drawText(posToDraw, textToDraw);
-
-
-    QFontMetrics fm(font);
-    int pixelsWide = fm.width(textToDraw);
-    int piexelsHigh = fm.height();
-    // qDebug() << pixelsWide << piexelsHigh;
-    // qDebug() << width() ;
-
-    painter.drawText(QPoint(300, 100), QString::number(fps));
-//    painter.drawText(QPoint(350, 100), QString::number(frameCount));
-//    painter.drawText(QPoint(400, 100), QString::number(frameTime.elapsed()));
-
-    // leave draw context
-    painter.endNativePainting();
-
-    // swap gl buffer
-    swapBuffers();
+    // default draw function
+    // make if empty
+    // to ensure w_draw will be called correctly
 }
 
 void VRenderer::enterDraw(QPainter &painter)
@@ -99,65 +45,15 @@ void VRenderer::exitDraw(QPainter &painter)
 
 void VRenderer::w_draw()
 {
-    ++frameCount;
+    // ++frameCount;
 //    double fps = frameCount / ((double)frameTime.elapsed() / 1000.0);
     double dt = ((double)frameTime.elapsed() - lastFrameTiem )/ 1000.0;
     lastFrameTiem = frameTime.elapsed();
-    // mix gl and qpainter
     QPainter painter(this);
     enterDraw(painter);
-    // enter draw context
-    // painter.beginNativePainting();
-
-    // video draw stuffs
-    // handlePaintEvent();
-
-    // custom draw stuffs
-    // ---- draw normal text
-//    int alpha = 100;
-//    painter.setPen(QColor(100, 100, 200, 255));
-//    QFont font;
-//    font.setPixelSize(24);
-//    painter.setFont(font);
-//    painter.setRenderHints(QPainter::Antialiasing | QPainter::TextAntialiasing);
-//    painter.drawText(10, 10, "Hello World");
-//    painter.drawLine(0, 0, 400, 400);
-//    painter.drawText(100, 100, "你好世界");
-//    // ---- draw text with outline
-//    QPainterPath path;
-//    QPen pen;
-//    QPoint posToDraw(50, 132);
-//    font.setWeight(QFont::Weight::Normal);
-//    pen.setWidth(5);
-//    pen.setColor(QColor(0, 0, 0, alpha));
-//    painter.setFont(font);
-//    painter.setPen(pen);
-
-//    const QString textToDraw("中文And English with Σ(*ﾟдﾟﾉ)ﾉ...");
-//    path.addText(posToDraw,font, textToDraw);
-//    painter.drawPath(path);
-//    pen.setColor(QColor(255, 255, 255, alpha));
-//    painter.setPen(pen);
-//    painter.drawText(posToDraw, textToDraw);
-
-
-//    QFontMetrics fm(font);
-//    int pixelsWide = fm.width(textToDraw);
-//    int piexelsHigh = fm.height();
-    // qDebug() << pixelsWide << piexelsHigh;
-    // qDebug() << width() ;
 
     LuaManager::instance()->callUpdateFunc(dt);
     LuaManager::instance()->callDrawFunc(painter);
 
-//    painter.drawText(QPoint(30, 50), QString::number(int(fps)) + " FPS");
-//    painter.drawText(QPoint(350, 100), QString::number(frameCount));
-//    painter.drawText(QPoint(400, 100), QString::number(frameTime.elapsed()));
-
-    // leave draw context
-    // painter.endNativePainting();
-
-    // swap gl buffer
-    // swapBuffers();
     exitDraw(painter);
 }
